8b23c248df0dfb4e27e2fa72a3458b21bad09d32
MorPeindaDiop <61980976+MorPeindaDiop@users.noreply.github.com>
2020-09-23 12:16:51 +0200

    modifiche

--- src/app/features/cart/cart-routing.module.ts
+++ src/app/features/cart/cart-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CartComponent } from './main/cart.component';
+import { SpedizioneComponent } from './spedizione/spedizione.component';
 
-const routes: Routes = [{ path: '', component: CartComponent }];
+const routes: Routes = [{ path: '', component: CartComponent },
+{ path: 'spedizione', component: SpedizioneComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
